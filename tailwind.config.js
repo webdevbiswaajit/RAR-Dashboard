17801defcae408e4cdb9aea90165d40292db59cf
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-12-11 02:34:28 +0600

    Login Page Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Nunito: ["Nunito"],
+      },
+      colors: {
+        black_light: "#0F172A;",
+        Purple_blue: "#9333EA;;",
+        Ash: "#818181",
+        Orenge: "#FF0000",
+        Light_ash: " #FAF5FF;",
+      },
+      margin: {
+        15: "60px",
+      },
+    },
   },
   plugins: [],
 };
